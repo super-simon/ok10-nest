4430d27e9e8209a3663ea6a59cd88c6d33b8e3e2
Oleksandr Stetsiuk <oleksandr.v.stetsiuk@gmail.com>
2023-07-26 19:05:05 +0300

    postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,19 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AnimalModule } from './animal/animal.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmConfiguration } from './config/database/type-orm-configuration';
 import { UserController } from './user/user.controller';
 import { UserModule } from './user/user.module';
 import { UserService } from './user/user.service';
 
 @Module({
-  imports: [UserModule, AnimalModule],
+  imports: [
+    TypeOrmModule.forRootAsync(TypeOrmConfiguration.config),
+    UserModule,
+    AnimalModule,
+  ],
   controllers: [AppController, UserController],
   providers: [AppService, UserService],
 })
